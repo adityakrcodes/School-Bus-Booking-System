e7ec018f1402940a5d4dadca905cfd1ca186b01e
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-03-31 19:14:27 +0530

    REMOVED trash code

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,4 @@
 function checkForm() {
-    fetch("check.php")
-        .then((response) => {
-            if(!response.ok){ // Before parsing (i.e. decoding) the JSON data,
-                              // check for any errors.
-                // In case of an error, throw.
-                throw new Error("Something went wrong!");
-            }
-
-            return response.json(); // Parse the JSON data.
-        })
-        .then((data) => {
-             // This is where you handle what to do with the response.
-             alert(data); // Will alert: 42
-        })
-        .catch((error) => {
-             // This is where you handle errors.
-        });
     errorText = document.getElementById("errorText");
     if (
 		document.getElementById("password").value !=
